40cf17fe8e62ef8b918db3191d9aaff2239b364a
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-10-08 18:21:20 -0400

    Update Screens & Components
    foi adicionado a tela de Search Bar e Biblioteca de musicas apenas para representação posteriormente

---
+++ screens/LibraryScreen/index.js
@@ -0,0 +1,83 @@
+import FooterMenu from "../../components/FooterMenu";
+import Header from "../../components/Header";
+import { Body, Container, Texto } from "./styles";
+
+const LibraryScreen = () => {
+  return (
+    <>
+      <Body>
+        <Header />
+        <Container>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+          <Texto>teste</Texto>
+        </Container>
+        <FooterMenu />
+      </Body>
+    </>
+  );
+};
+
+export default HomeScreen;
---
+++ screens/LibraryScreen/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Body = styled.View`
+  flex: 1;
+  background-color: #121212;
+`;
+
+export const Container = styled.ScrollView`
+  flex: 1;
+`;
+
+export const Texto = styled.Text`
+  color: white;
+`;
